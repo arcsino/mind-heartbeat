4c05f18e213881447149ff77bd3824b79643163a
arcsino <173187432+arcsino@users.noreply.github.com>
2025-12-13 23:16:23 +0900

    ユーザー認証画面のパスを設定

--- mind_heartbeat/accounts/urls.py
+++ mind_heartbeat/accounts/urls.py
@@ -1,23 +1,42 @@
 from django.urls import path
 
 from .views import (
+    DeleteUserView,
     LoginAPIView,
+    LoginView,
     LogoutAPIView,
+    LogoutView,
+    NicknameUpdateView,
     PasswordChangeAPIView,
+    PasswordChangeView,
+    ProfileView,
+    SignupView,
     UserDeleteAPIView,
     UserDetailAPIView,
     UserRegistrationAPIView,
     UserUpdateAPIView,
 )
 
+app_name = "accounts"
 urlpatterns = [
+    # page views
     # Any user can access.
-    path(route="registration/", view=UserRegistrationAPIView.as_view()),
-    path(route="login/", view=LoginAPIView.as_view()),
+    path("signup/", SignupView.as_view(), name="signup"),
+    path("login/", LoginView.as_view(), name="login"),
     # Authenticated users only can access.
-    path(route="detail/", view=UserDetailAPIView.as_view()),
-    path(route="update/", view=UserUpdateAPIView.as_view()),
-    path(route="password-change/", view=PasswordChangeAPIView.as_view()),
-    path(route="logout/", view=LogoutAPIView.as_view()),
-    path(route="delete/", view=UserDeleteAPIView.as_view()),
+    path("profile/", ProfileView.as_view(), name="profile"),
+    path("nickname-update/", NicknameUpdateView.as_view(), name="nickname-update"),
+    path("password-change/", PasswordChangeView.as_view(), name="password-change"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("delete/", DeleteUserView.as_view(), name="delete"),
+    # API views
+    # Any user can access.
+    path(route="api/registration/", view=UserRegistrationAPIView.as_view()),
+    path(route="api/login/", view=LoginAPIView.as_view()),
+    # Authenticated users only can access.
+    path(route="api/detail/", view=UserDetailAPIView.as_view()),
+    path(route="api/update/", view=UserUpdateAPIView.as_view()),
+    path(route="api/password-change/", view=PasswordChangeAPIView.as_view()),
+    path(route="api/logout/", view=LogoutAPIView.as_view()),
+    path(route="api/delete/", view=UserDeleteAPIView.as_view()),
 ]
