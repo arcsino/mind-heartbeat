8a5fecd62afb7c553723d77b425d30cce0f1e2b3
arcsino <173187432+arcsino@users.noreply.github.com>
2025-12-13 20:38:31 +0900

    ユーザ認証用URLパターンの追加

--- mind_heartbeat/mind_heartbeat/urls.py
+++ mind_heartbeat/mind_heartbeat/urls.py
@@ -24,7 +24,7 @@ from . import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path(route="", view=include("feelings.urls")),
-    path(route="auth-api/", view=include("accounts.urls")),
+    path(route="auth/", view=include("accounts.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
