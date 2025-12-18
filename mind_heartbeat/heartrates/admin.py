12c91c5b76f47abfe3d5f5265104ee84fed823ae
arcsino <173187432+arcsino@users.noreply.github.com>
2025-12-19 00:32:49 +0900

    心拍数関連の管理者サイトの表示を追加

--- mind_heartbeat/heartrates/admin.py
+++ mind_heartbeat/heartrates/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import HeartRate, WearOSIntegration
+
+
+@admin.register(WearOSIntegration)
+class WearOSIntegrationAdmin(admin.ModelAdmin):
+    list_display = ("user", "created_at", "updated_at")
+    search_fields = ("user__username",)
+    readonly_fields = ("user", "token_hash", "created_at", "updated_at")
+
+
+@admin.register(HeartRate)
+class HeartRateAdmin(admin.ModelAdmin):
+    list_display = ("user", "bpm", "timestamp")
+    search_fields = ("user__username",)
